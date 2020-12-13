a20d04fad0de05625393a21de718138eada2dab7
Stafford Smith <staffordsmith83@gmail.com>
2020-12-13 17:27:26 +0800

    Reading in local geojson file now... Change filename to farms.geojson... Not extracting names from the features array though...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-gmaps-demo';
-  // lat = 51.678418;
-  // lng = 7.809007;
+
   map: any; // important to declare the type of property map, so we can refer to it with this.map
-  features: any;
+  features: any;  // Do I need this too?
   
 
   onMapReady(map: google.maps.Map) {
 
     this.map = map;
-    this.map.setCenter({lat:-32, lng:127});
-    this.map.setZoom(10);
-   
-    //need to define the type of features?
-    // let features: any; //doesnt work like this...
+    this.map.setCenter({lat:-32.910, lng:117.133});
+    this.map.setZoom(10); 
 
-    this.map.data.loadGeoJson('http://localhost:4200/assets/sample-farms.geojson', {}, 
-      function (features: any) {
-      console.log(features);
-    });
+    // Error in this method call?
+    // Had to change sample-farms.geojson to farms.geojson... Was getting 400 error...
+    this.map.data.loadGeoJson('http://localhost:4200/assets/farms.geojson', {}, 
+    function (features: any) {
+    console.log(features);
+  });
 
   }
 }
