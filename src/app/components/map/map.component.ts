73acb6b6d64e7c133e26098a928c1b5040b4cf88
Stafford Smith <staffordsmith83@gmail.com>
2020-12-23 08:39:58 +0800

    Working map doesnt overflow

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -4,87 +4,74 @@ import * as mapboxgl from 'mapbox-gl';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnInit {
+  // Declare Types
   map: mapboxgl.Map | undefined;
-  // style = 'mapbox://styles/mapbox/streets-v11';
   lat = -17.9644;
   lng = 122.2304;
-  constructor() { }
+  constructor() {}
   ngOnInit() {
-    // Declare Types
-    
-    
     // mapboxgl.accessToken = environment.mapbox.accessToken;
     let map = new mapboxgl.Map({
       container: 'map',
       style: {
-        'version': 8,
-        'sources': {
-        'raster-tiles': {
-        'type': 'raster',
-        'tiles': [
-        'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg'
+        version: 8,
+        sources: {
+          'raster-tiles': {
+            type: 'raster',
+            tiles: [
+              'https://stamen-tiles.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg',
+            ],
+            tileSize: 256,
+            attribution:
+              'Map tiles by <a target="_top" rel="noopener" href="http://stamen.com">Stamen Design</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>',
+          },
+        },
+        layers: [
+          {
+            id: 'simple-tiles',
+            type: 'raster',
+            source: 'raster-tiles',
+            minzoom: 0,
+            maxzoom: 22,
+          },
         ],
-        'tileSize': 256,
-        'attribution':
-        'Map tiles by <a target="_top" rel="noopener" href="http://stamen.com">Stamen Design</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>, under <a target="_top" rel="noopener" href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>'
-        }
-        },
-        'layers': [
-        {
-        'id': 'simple-tiles',
-        'type': 'raster',
-        'source': 'raster-tiles',
-        'minzoom': 0,
-        'maxzoom': 22
-        }
-        ]
-        },
+      },
       zoom: 11,
-      center: [this.lng, this.lat]
-  });
-  // Add map controls
+      center: [this.lng, this.lat],
+    });
+    // Add map controls
     map.addControl(new mapboxgl.NavigationControl());
 
-
     map.on('load', function () {
-      
       map.addSource('nidem', {
-      'type': 'raster',
-      'tiles': ['http://ec2-13-55-247-227.ap-southeast-2.compute.amazonaws.com:8080/geoserver/NIDEM/wms?service=WMS&version=1.1.0&request=GetMap&LAYERS=NIDEM_mosaic&SRS=epsg:3857&BBOX={bbox-epsg-3857}&WIDTH=256&HEIGHT=256&FORMAT=image/png&transparent=TRUE']
+        type: 'raster',
+        tiles: [
+          'http://ec2-13-55-247-227.ap-southeast-2.compute.amazonaws.com:8080/geoserver/NIDEM/wms?service=WMS&version=1.1.0&request=GetMap&LAYERS=NIDEM_mosaic&SRS=epsg:3857&BBOX={bbox-epsg-3857}&WIDTH=256&HEIGHT=256&FORMAT=image/png&transparent=TRUE',
+        ],
       });
-      map.addLayer(
-      {
-      'id': 'nidem_wms',
-      'type': 'raster',
-      'source': 'nidem',
-      'paint': {}
-
-      },
-
-      );
+      map.addLayer({
+        id: 'nidem_wms',
+        type: 'raster',
+        source: 'nidem',
+        paint: {},
       });
+    });
 
     // Add the NIDEM source
     // Correct WMS URl with dynamic bbox
     // 'http://ec2-13-55-247-227.ap-southeast-2.compute.amazonaws.com:8080/geoserver/NIDEM/wms?service=WMS&version=1.1.0&request=GetMap&LAYERS=NIDEM_mosaic&SRS=epsg:3857&BBOX={bbox-epsg-3857}&WIDTH=256&HEIGHT=256&FORMAT=image/png'
-
   }
 }
 
-
-
-
-
-// /* TODO: 
+// /* TODO:
 // Get feature name and other attributes from the user, like in ArcGIS
 // Use the new attribute-form component, to let users enter a full range of attributes
 // The attribute field list should be extracted fromt he layer you are currently editing
 // And the form dynamically built
 // */
-
 
 // import { GoogleMapsAPIWrapper } from '@agm/core';
 // import { MVCArray } from '@agm/core/services/google-maps-types';
@@ -134,7 +121,7 @@ export class MapComponent implements OnInit {
 //           // get the feature geometry, which is of type google.maps.Data.Geometry
 //           // then typecast it to a google maps data polygon,
 //           // const polygon = feature.getGeometry() as google.maps.Data.Polygon;
-//           // THIS DIDNT SEEM TO BE NECESSARY, AND LETS US DEAL WITH ALL SHAPE TYPES? 
+//           // THIS DIDNT SEEM TO BE NECESSARY, AND LETS US DEAL WITH ALL SHAPE TYPES?
 //           const geometry = feature.getGeometry();
 //           geometry.forEachLatLng((LatLng) => {
 //             bbox.extend(LatLng);
@@ -180,29 +167,29 @@ export class MapComponent implements OnInit {
 //         // disable drawing manager
 //         this.drawingManager?.setDrawingMode(null);
 
-//         // Get feature name from user   
+//         // Get feature name from user
 //         const featureName: string = prompt('What is the name of the feature?');
-        
+
 //         // Setup the variables that we need to scope both inside and ouside out case statements
 //         let feature: google.maps.Data.Feature;
 //         let geom: google.maps.Data.Geometry | undefined;  // this is the parent class of Point, Linestring, and Polygon
 
 //         // Build a different google maps data object depending on the type of drawing we made
 //         switch (event.type) {
-//           case google.maps.drawing.OverlayType.MARKER:            
-//             geom = new google.maps.Data.Point(event.overlay.getPosition());  
+//           case google.maps.drawing.OverlayType.MARKER:
+//             geom = new google.maps.Data.Point(event.overlay.getPosition());
 //           break;
 
-//           case google.maps.drawing.OverlayType.POLYLINE:            
-//             geom = new google.maps.Data.LineString(event.overlay.getPath().getArray());  
+//           case google.maps.drawing.OverlayType.POLYLINE:
+//             geom = new google.maps.Data.LineString(event.overlay.getPath().getArray());
 //           break;
 
-//           case google.maps.drawing.OverlayType.POLYGON:            
-//             geom = new google.maps.Data.Polygon([event.overlay.getPath().getArray()]);  
+//           case google.maps.drawing.OverlayType.POLYGON:
+//             geom = new google.maps.Data.Polygon([event.overlay.getPath().getArray()]);
 //           break;
 
-//         } 
-        
+//         }
+
 //         // Build the feature and add to the map and TOC as long as we have one of the valid geometries
 //         if (geom) {
 //           // Build the feature
@@ -212,14 +199,14 @@ export class MapComponent implements OnInit {
 //               name: featureName
 //             }
 //           })
-          
+
 //           // Add it to the map
 //           this.map?.data.add(feature);
-  
+
 //           // push the farm name to our list of feature names, which will be dynamically reflected in the sidebar component
 //           const farmName = feature.getProperty('name');
 //           farms.push({ farmName });
-          
+
 //         } else {
 //           console.log("Not a valid feature, not adding to TOC.")
 //         }
