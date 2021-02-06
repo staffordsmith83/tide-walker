e9540ddb97d0beb61c091318c762373bfb5f3d2e
Stafford Smith <staffordsmith83@gmail.com>
2021-02-06 20:11:09 +0800

    Tides requst working and displaying to console... Next thing get the map component to update too...

--- src/app/components/tide-input-basic/tide-input-basic.component.ts
+++ src/app/components/tide-input-basic/tide-input-basic.component.ts
@@ -26,7 +26,7 @@ export class TideInputBasicComponent implements OnInit {
   //set the custom event
   @Output() tideUpdated = new EventEmitter<string>(); //Maybe unecessary???? Or maybe newTideHeight updater method is unecessary?
   newTideHeight = '';
-  newDateTime: number = 0;
+  newDateTime: number = 1612612330;
   hintLabelText: string = 'Enter a decimal value from -10.0 to 10.0';
   placeholderText: string = 'enter tide height';
   @Input() matDatepicker: Date | undefined;
@@ -40,37 +40,10 @@ export class TideInputBasicComponent implements OnInit {
     this.tideUpdated.emit(this.newTideHeight);
   }
 
-  ngOnInit() {
-  //   // Create observable
-  //   const tideHeight = new Observable<number>(getTideHeightFromDateTime);
-
-  //   // Execute the Observable and print the
-  //   // result of each notification
-  //   // next() is a call to countOnetoTen method
-  //   // to get the next value from the observable
-  //   tideHeight.subscribe({
-  //     next(num) {
-  //       console.log(num);
-  //     },
-  //   });
-
-  //   // This function runs when subscribe()
-  //   // is called
-  //   function getTideHeightFromDateTime(observer: Observer<number>) {
-  //     [-4, -3, -2, 1, 0, 1, 2, 3, 4].forEach((tide, i) => {
-  //       setTimeout(() => {
-  //         observer.next(tide);
-  //       }, i * 1000);
-  //     });
-
-  //     // // Unsubscribe after completing
-  //     // // the sequence
-  //     // return { unsubscribe() {} };
-  //   }
-  }
+  ngOnInit() {}
 
   calculateTide(dateTime: number ): void {
-    // console.log("Event recieved is: " + event);    
+    console.log("Sending Epoch Time to Calculations Service: " + dateTime);    
     let tideCalculated = this.CalculationsService.getHeightFromDateTime(
       dateTime
     );
--- src/app/services/calculations.service.ts
+++ src/app/services/calculations.service.ts
@@ -8,27 +8,35 @@ import { Observable, Subscription, of } from 'rxjs';
 })
 export class CalculationsService {
   // include methods to calculate tideHeight from day/time
+  // TODO: also return station name that observation is from. Display to user.
+
   worldTidesApiKey = '3c2d3af9-c54c-42cf-8366-9cd62931d8d5';
   requestHeader =
-    'https://www.worldtides.info/api/v2?heights&lat=33.768321&lon=-118.195617&key=3c2d3af9-c54c-42cf-8366-9cd62931d8d5&step=60&length=1&start=';
+    'https://www.worldtides.info/api/v2?heights&lat=-18.061&lon=122.369&key=3c2d3af9-c54c-42cf-8366-9cd62931d8d5&stationDistance=100&step=60&length=1&start=';
   tideHeight: number = 999999;
 
   constructor(private http: HttpClient) {}
 
+  getNearestTideStation() {
+  }
+
   getHeightFromDateTime(dateTime: number) {
     let tideHeight: number;
     let response: Observable<any>;
-    let request: string = this.requestHeader + dateTime.toString;
+    let request: string = this.requestHeader + dateTime.toString();
+    console.log("Calculations Service received datTime as:" + dateTime.toString())
 
 
-    let data = this.http
+    this.http
       .get<any>(request)
-      .toPromise()
-      .then((result) => {
-        console.log('From Promise:', result.heights[0].height);
-        return result.heights[0].height;
+      .subscribe((result) => {
+        console.log('From WorldTides:', result.heights[0].height);
       });
 
+    console.log(request);
+
   }
+
+  
 
 }
