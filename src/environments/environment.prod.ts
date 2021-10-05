5222b56b8c034d4bf6612e0232abd75bd606d039
Stafford Smith <stafford.smith@ngis.com.au>
2021-10-05 23:09:46 +0800

    Changed to new GCP hosted geoserver

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   baseApiUrl: '',
-  geoServerRoot: 'ec2-54-206-212-225.ap-southeast-2.compute.amazonaws.com:8080/geoserver',
+  geoServerRoot: '34.87.202.161:8080/geoserver',
+  // geoServerRoot: 'ec2-54-206-212-225.ap-southeast-2.compute.amazonaws.com:8080/geoserver',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   baseApiUrl: '',
-  geoServerRoot: 'ec2-54-206-212-225.ap-southeast-2.compute.amazonaws.com:8080/geoserver',
+  geoServerRoot: '34.87.202.161:8080/geoserver',
+  // geoServerRoot: 'ec2-54-206-212-225.ap-southeast-2.compute.amazonaws.com:8080/geoserver',
 };
 
 /*
