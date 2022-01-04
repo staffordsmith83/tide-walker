e411eaee80ca7eca29fdd07bbda14997c349ad80
Stafford Smith <stafford.smith@ngis.com.au>
2022-01-04 13:45:47 +0800

    Updated server with elastic IP

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   baseApiUrl: '',
-  geoServerRoot: 'http://ec2-3-25-96-54.ap-southeast-2.compute.amazonaws.com:8080/geoserver',
+  // geoServerRoot: 'http://ec2-3-25-96-54.ap-southeast-2.compute.amazonaws.com:8080/geoserver',
+  geoServerRoot: 'http://13.238.73.166:8080/geoserver',
   nidemLayer: 'NIDEM',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   baseApiUrl: '',
-  geoServerRoot: 'http://ec2-3-25-96-54.ap-southeast-2.compute.amazonaws.com:8080/geoserver',
+  // geoServerRoot: 'http://ec2-3-25-96-54.ap-southeast-2.compute.amazonaws.com:8080/geoserver',
+  geoServerRoot: 'http://13.238.73.166:8080/geoserver',
   nidemLayer: 'NIDEM',
 };
